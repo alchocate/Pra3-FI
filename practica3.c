afffe33532278d070a3b579958e4dfd6ad293307
alanfarre <alanfarre@PCALAN.>
2024-11-18 09:54:21 +0100

    Exercici 6

--- practica3.c
+++ practica3.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main(){
-        int x=3,res;
-	char a='a';
-	res=a*x;
+        int i;
+	int a[4]={1,2,3,4};
+	int b[4]={5,6,7,8};
+	int c[4];
+	while (i<4){
+		c[i]=a[i]*b[i];
+		i++;
+	}
+	return 0;
 }
