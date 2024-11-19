effb1ff7910e703d86608d010e15fc1e075b8da5
Your Nme <ericcasanova.25@gmail.com>
2024-11-19 23:12:18 +0100

    Exercici 5

--- practica3.c
+++ practica3.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
-int main(){
-        int i;
-	int a[4]={1,2,3,4};
-	int b[4]={5,6,7,8};
-	int c[4];
-	while (i<4){
-		c[i]=a[i]*b[i];
-		i++;
-	}
-	return 0;
-}
+int main(){ 
+	char a[3]; 
+	a[0] = 1; 
+	a[1] = 2; 
+	a[2] = 3; 
+	return 0; 
+} 
